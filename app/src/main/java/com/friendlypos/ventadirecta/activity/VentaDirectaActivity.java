88089f59d13347d491be15e43e83377cc1c7d96d
DelvoM <oscardelvo25@gmail.com>
2018-08-13 16:14:18 -0600

    Commit107

---
+++ app/src/main/java/com/friendlypos/ventadirecta/activity/VentaDirectaActivity.java
@@ -0,0 +1,377 @@
+package com.friendlypos.ventadirecta.activity;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MenuItem;
+
+import com.friendlypos.R;
+import com.friendlypos.application.bluetooth.PrinterService;
+import com.friendlypos.application.util.Functions;
+import com.friendlypos.distribucion.fragment.BaseFragment;
+import com.friendlypos.distribucion.modelo.Pivot;
+import com.friendlypos.distribucion.modelo.invoice;
+import com.friendlypos.distribucion.modelo.sale;
+import com.friendlypos.distribucion.util.Adapter;
+import com.friendlypos.preventas.delegate.PreSellInvoiceDelegate;
+import com.friendlypos.preventas.fragment.PrevResumenFragment;
+import com.friendlypos.preventas.fragment.PrevSelecClienteFragment;
+import com.friendlypos.preventas.fragment.PrevSelecProductoFragment;
+import com.friendlypos.preventas.fragment.PrevTotalizarFragment;
+import com.friendlypos.preventas.modelo.invoiceDetallePreventa;
+import com.friendlypos.principal.activity.BluetoothActivity;
+import com.friendlypos.principal.activity.MenuPrincipal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.ButterKnife;
+
+public class VentaDirectaActivity extends BluetoothActivity {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private int invoiceIdPreventa;
+    private String metodoPagoClientePreventa;
+    private String creditoLimiteClientePreventa = null;
+    private int selecClienteTabPreventa;
+    private String dueClientePreventa;
+
+    private double totalizarSubGrabado;
+    private double totalizarSubExento;
+    private double totalizarSubTotal;
+    private double totalizarDescuento;
+    private double totalizarImpuestoIVA;
+    private double totalizarTotal;
+    private double totalizarTotalDouble;
+
+    private PreSellInvoiceDelegate preSellInvoiceDelegate;
+
+    public int getInvoiceIdPreventa() {
+        return invoiceIdPreventa;
+    }
+
+    public void setInvoiceIdPreventa(int invoiceIdPreventa) {
+        this.invoiceIdPreventa = invoiceIdPreventa;
+    }
+
+
+    public double getTotalizarSubGrabado() {
+        return totalizarSubGrabado;
+    }
+
+    public void setTotalizarSubGrabado(double totalizarSubGrabado) {
+        this.totalizarSubGrabado = this.totalizarSubGrabado + totalizarSubGrabado;
+    }
+
+    public double getTotalizarSubExento() {
+        return totalizarSubExento;
+    }
+
+    public void setTotalizarSubExento(double totalizarSubExento) {
+        this.totalizarSubExento = this.totalizarSubExento + totalizarSubExento;
+    }
+
+    public double getTotalizarSubTotal() {
+        return totalizarSubTotal;
+    }
+
+    public void setTotalizarSubTotal(double totalizarSubTotal) {
+        this.totalizarSubTotal = this.totalizarSubTotal + totalizarSubTotal;
+    }
+
+    public double getTotalizarDescuento() {
+        return totalizarDescuento;
+    }
+
+    public void setTotalizarDescuento(double totalizarDescuento) {
+        this.totalizarDescuento = this.totalizarDescuento + totalizarDescuento;
+    }
+
+    public double getTotalizarImpuestoIVA() {
+        return totalizarImpuestoIVA;
+    }
+
+    public void setTotalizarImpuestoIVA(double totalizarImpuestoIVA) {
+        this.totalizarImpuestoIVA = this.totalizarImpuestoIVA + totalizarImpuestoIVA;
+    }
+
+    public double getTotalizarTotal() {
+        return totalizarTotal;
+    }
+
+    public void setTotalizarTotal(double totalizarTotal) {
+        this.totalizarTotal = this.totalizarTotal + totalizarTotal;
+    }
+
+
+    public String getMetodoPagoClientePreventa() {
+        return metodoPagoClientePreventa;
+    }
+
+    public void setMetodoPagoClientePreventa(String metodoPagoClientePreventa) {
+        this.metodoPagoClientePreventa = metodoPagoClientePreventa;
+    }
+
+    public String getCreditoLimiteClientePreventa() {
+        return creditoLimiteClientePreventa;
+    }
+
+    public void setCreditoLimiteClientePreventa(String creditoLimiteClientePreventa) {
+        this.creditoLimiteClientePreventa = creditoLimiteClientePreventa;
+    }
+
+    public int getSelecClienteTabPreventa() {
+        return selecClienteTabPreventa;
+    }
+
+    public void setSelecClienteTabPreventa(int selecClienteTabPreventa) {
+        this.selecClienteTabPreventa = selecClienteTabPreventa;
+    }
+
+    public String getDueClientePreventa() {
+        return dueClientePreventa;
+    }
+
+    public void setDueClientePreventa(String dueClientePreventa) {
+        this.dueClientePreventa = dueClientePreventa;
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ventadirecta);
+        ButterKnife.bind(this);
+        toolbar = (Toolbar) findViewById(R.id.toolbarVentaDirecta);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        preSellInvoiceDelegate = new PreSellInvoiceDelegate(this);
+        connectToPrinter();
+        /*toolbar = (Toolbar) findViewById(R.id.toolbarDistribucion);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);*/
+
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.viewpagerVentaDirecta);
+        if (viewPager != null) {
+            setupViewPager(viewPager);
+        }
+
+        tabLayout = (TabLayout) findViewById(R.id.tabsVentaDirecta);
+        tabLayout.setupWithViewPager(viewPager);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int tabCliente = getSelecClienteTabPreventa();
+                if (tabCliente == 0 && tab.getPosition() != 0) {
+
+                    Functions.CreateMessage(VentaDirectaActivity.this, "Venta Directa", "Seleccione una factura.");
+
+                    new Handler().postDelayed(
+                            new Runnable() {
+
+                                @Override
+                                public void run() {
+                                    tabLayout.getTabAt(0).select();
+                                }
+                            }, 100);
+                }
+                else {
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+    }
+
+
+    private void connectToPrinter() {
+        //if(bluetoothStateChangeReceiver.isBluetoothAvailable()) {
+        getPreferences();
+        if (printer_enabled) {
+            if (printer == null || printer.equals("")) {
+//                AlertDialog d = new AlertDialog.Builder(context)
+//                        .setTitle(getResources().getString(R.string.printer_alert))
+//                        .setMessage(getResources().getString(R.string.message_printer_not_found))
+//                        .setNegativeButton(getString(android.R.string.ok), null)
+//                        .show();
+            }
+            else {
+                if (!isServiceRunning(PrinterService.CLASS_NAME)) {
+                    PrinterService.startRDService(getApplicationContext(), printer);
+                }
+            }
+        }
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+
+        Adapter adapter = new Adapter(getSupportFragmentManager());
+        final List<BaseFragment> list = new ArrayList<>();
+        list.add(new PrevSelecClienteFragment());
+        list.add(new PrevSelecProductoFragment());
+        list.add(new PrevResumenFragment());
+        list.add(new PrevTotalizarFragment());
+        adapter.addFragment(list.get(0), "Seleccionar Cliente");
+        adapter.addFragment(list.get(1), "Seleccionar Productos");
+        adapter.addFragment(list.get(2), "Resumen");
+        adapter.addFragment(list.get(3), "Totalizar");
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                list.get(position).updateData();
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+
+                Intent intent = new Intent(VentaDirectaActivity.this, MenuPrincipal.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                finish();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    //Check if the printing service is running
+    public boolean isServiceRunning(String serviceClassName) {
+        final ActivityManager activityManager = (ActivityManager) getApplicationContext().getSystemService(Context.ACTIVITY_SERVICE);
+        final List<ActivityManager.RunningServiceInfo> services = activityManager.getRunningServices(Integer.MAX_VALUE);
+
+        for (ActivityManager.RunningServiceInfo runningServiceInfo : services) {
+            if (runningServiceInfo.service.getClassName().equals(serviceClassName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public invoiceDetallePreventa getCurrentInvoice() {
+        return preSellInvoiceDelegate.getCurrentInvoice();
+    }
+
+    public sale getCurrentVenta() {
+        return preSellInvoiceDelegate.getCurrentVenta();
+    }
+
+    public invoice getInvoiceByInvoiceDetalles() {
+        return preSellInvoiceDelegate.getInvoiceByInvoiceDetalle();
+    }
+
+
+
+
+    public List<Pivot> getAllPivotDelegate() {
+        return preSellInvoiceDelegate.getAllPivot();
+    }
+
+    public void insertProduct(Pivot pivot) {
+        preSellInvoiceDelegate.insertProduct(pivot);
+    }
+
+    public void borrarProduct(Pivot pivot) {
+        preSellInvoiceDelegate.borrarProducto(pivot);
+    }
+
+    public void initCurrentInvoice(String id, String branch_office_id, String numeration, double latitude, double longitude,
+                                   String date, String times, String date_presale, String times_presale, String due_data,
+                                   String invoice_type_id, String payment_method_id, String totalSubtotal, String totalGrabado,
+                                   String totalExento, String totalDescuento, String percent_discount, String totalImpuesto,
+                                   String totalTotal, String changing, String notes, String canceled,
+                                   String paid_up, String paid, String created_at, String idUsuario,
+                                   String idUsuarioAplicado) {
+        preSellInvoiceDelegate.initInvoiceDetallePreventa(id, branch_office_id, numeration, latitude, longitude,
+                date, times, date_presale, times_presale, due_data,
+                invoice_type_id, payment_method_id, totalSubtotal, totalGrabado,
+                totalExento, totalDescuento, percent_discount, totalImpuesto,
+                totalTotal, changing, notes, canceled,
+                paid_up, paid, created_at, idUsuario,
+                idUsuarioAplicado);
+    }
+
+    public void initCurrentVenta(String p_id, String p_invoice_id, String p_customer_id, String p_customer_name,
+                                 String p_cash_desk_id, String p_sale_type, String p_viewed, String p_applied,
+                                 String p_created_at, String p_updated_at, String p_reserved, int aplicada, int subida, int facturaDePreventa) {
+        preSellInvoiceDelegate.initVentaDetallesPreventa(p_id, p_invoice_id, p_customer_id, p_customer_name,
+                p_cash_desk_id, p_sale_type, p_viewed, p_applied,
+                p_created_at, p_updated_at, p_reserved, aplicada, subida, facturaDePreventa);
+    }
+
+    public void initProducto(int pos) {
+        preSellInvoiceDelegate.initProduct(pos);
+    }
+
+    public void cleanTotalize() {
+        totalizarSubGrabado = 0.0;
+        totalizarSubExento = 0.0;
+        totalizarSubTotal = 0.0;
+        totalizarDescuento = 0.0;
+        totalizarImpuestoIVA = 0.0;
+        totalizarTotal = 0.0;
+        totalizarTotalDouble = 0.0;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        preSellInvoiceDelegate.destroy();
+        preSellInvoiceDelegate = null;
+    }
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK)) {
+            Intent intent = new Intent(VentaDirectaActivity.this, MenuPrincipal.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            finish();
+            Log.d("ATRAS", "Atras");
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
+
---
+++ app/src/main/java/com/friendlypos/ventadirecta/adapters/VentaDirClienteAdapter.java
@@ -0,0 +1,276 @@
+package com.friendlypos.ventadirecta.adapters;
+
+/**
+ * Created by DelvoM on 13/08/2018.
+ */
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import com.friendlypos.R;
+import com.friendlypos.application.util.Functions;
+import com.friendlypos.distribucion.modelo.Pivot;
+import com.friendlypos.distribucion.modelo.invoice;
+import com.friendlypos.preventas.activity.PreventaActivity;
+import com.friendlypos.preventas.modelo.invoiceDetallePreventa;
+import com.friendlypos.principal.modelo.Clientes;
+import com.friendlypos.ventadirecta.activity.VentaDirectaActivity;
+
+import java.util.List;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
+
+public class VentaDirClienteAdapter extends RecyclerView.Adapter<VentaDirClienteAdapter.CharacterViewHolder> {
+
+    public List<Clientes> contentList;
+    private VentaDirectaActivity activity;
+    private int selected_position = -1;
+    private static Context QuickContext = null;
+    int tabCliente;
+    int activa = 0;
+    String metodoPagoId;
+
+    private static Double creditolimite = 0.0;
+    private static Double descuentoFixed = 0.0;
+    private static Double cleintedue = 0.0;
+    private static Double credittime = 0.0;
+
+
+    public VentaDirClienteAdapter(Context context, VentaDirectaActivity activity, List<Clientes> contentList) {
+        this.contentList = contentList;
+        this.activity = activity;
+        this.QuickContext = context;
+    }
+
+    @Override
+    public VentaDirClienteAdapter.CharacterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.lista_ventadirecta_clientes, parent, false);
+
+        return new VentaDirClienteAdapter.CharacterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(VentaDirClienteAdapter.CharacterViewHolder holder, final int position) {
+        Clientes content = contentList.get(position);
+
+        creditolimite = Double.parseDouble(content.getCreditLimit());
+        descuentoFixed = Double.parseDouble(content.getFixedDiscount());
+        cleintedue = Double.parseDouble(content.getDue());
+        credittime = Double.parseDouble(content.getCreditTime());
+        final String cardCliente = content.getCard();
+        String companyCliente = content.getCompanyName();
+        String fantasyCliente = content.getFantasyName();
+
+        holder.txt_prev_card.setText(cardCliente);
+        holder.txt_prev_fantasyname.setText(fantasyCliente);
+        holder.txt_prev_companyname.setText(companyCliente);
+        holder.txt_prev_creditlimit.setText(String.format("%,.2f", (creditolimite)));
+        holder.txt_prev_fixeddescount.setText(String.format("%,.2f", (descuentoFixed)));
+        holder.txt_prev_due.setText(String.format("%,.2f", (cleintedue)));
+        holder.txt_prev_credittime.setText(String.format("%,.2f", (credittime)));
+
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                activa = 1;
+
+                final ProgressDialog progresRing = ProgressDialog.show(QuickContext, "Cargando", "Seleccionando Cliente", true);
+                progresRing.setCancelable(true);
+                new Thread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        try {
+                            Thread.sleep(5000);
+                        }
+                        catch (Exception e) {
+
+                        }
+                        progresRing.dismiss();
+                    }
+                }).start();
+
+                int pos = position;
+                if (pos == RecyclerView.NO_POSITION) return;
+
+                // Updating old as well as new positions
+                notifyItemChanged(selected_position);
+                selected_position = position;
+                notifyItemChanged(selected_position);
+
+                Clientes clickedDataItem = contentList.get(pos);
+
+                final String idCliente = clickedDataItem.getId();
+                final String nombreCliente = clickedDataItem.getName();
+                final int creditoTime = Integer.parseInt(clickedDataItem.getCreditTime());
+                final String creditoLimiteClienteP = clickedDataItem.getCreditLimit();
+                final String dueClienteP = clickedDataItem.getDue();
+
+                LayoutInflater layoutInflater = LayoutInflater.from(QuickContext);
+                View promptView = layoutInflater.inflate(R.layout.promptclient_preventa, null);
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(QuickContext);
+                alertDialogBuilder.setView(promptView);
+                final RadioButton rbcontado = (RadioButton) promptView.findViewById(R.id.contadoBill);
+                final RadioButton rbcredito = (RadioButton) promptView.findViewById(R.id.creditBill);
+
+                alertDialogBuilder
+                        .setCancelable(false)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+                            public void onClick(DialogInterface dialog, int id) {
+                                String fecha = Functions.getDate() + " " + Functions.get24Time();
+                                if (rbcredito.isChecked()) {
+                                    if (creditoTime == 0) {
+                                        Functions.CreateMessage(QuickContext, " ", "Este cliente no cuenta con crédito");
+                                    }
+                                    else {
+                                        // TRANSACCIÓN PARA ACTUALIZAR EL CAMPO METODO DE PAGO CREDITO DE LA FACTURA
+                                        metodoPagoId = "2";
+                                        Functions.CreateMessage(QuickContext, " ", "Se cambio la factura a crédito" + metodoPagoId);
+                                        notifyDataSetChanged();
+                                    }
+                                }
+                                else if (rbcontado.isChecked()) {
+                                    // TRANSACCIÓN PARA ACTUALIZAR EL CAMPO METODO DE PAGO CONTADO DE LA FACTURA
+                                    metodoPagoId = "1";
+                                    Functions.CreateMessage(QuickContext, " ", "Se cambio la factura a contado" + metodoPagoId);
+                                    notifyDataSetChanged();
+                                }
+
+                       /*    final Realm realm2 = Realm.getDefaultInstance();
+
+                            realm2.executeTransaction(new Realm.Transaction() {
+
+                                @Override
+                                public void execute(Realm realm) {
+
+                                    // increment index
+
+                                    int numero = (int) realm.where(invoice.class).max("id");
+
+                                    Number currentIdNum = numero;
+
+
+                                    if (currentIdNum == null) {
+                                        nextId = 1;
+                                    }
+                                    else {
+                                        nextId = currentIdNum.intValue() + 1;
+                                    }
+
+                                    // increment index
+                                    Number NumFactura = realm.where(invoice.class).max("numeration");
+
+                                    if (NumFactura == null) {
+                                        numeration = 1;
+                                    }
+                                    else {
+                                        numeration = NumFactura.intValue() + 1;
+                                    }
+
+                                }
+                           });
+*/
+                                //TODO MODIFICAR CON EL IDS CONSECUTIVOS (FACTURA Y NUMERACION)
+                                activity.initCurrentInvoice("1", "3", "00001", 0.0, 0.0, Functions.getDate(), Functions.get24Time(),
+                                        Functions.getDate(), Functions.get24Time(), Functions.getDate(), "2", metodoPagoId, "", "", "", "", "",
+                                        "","","","","","","",fecha,
+                                        "","");
+
+                                activity.initCurrentVenta("1", "1", idCliente, nombreCliente, "6", "2", "0", "0", fecha, fecha, "0", 1 , 0, 1);
+                                tabCliente = 1;
+                                activity.setSelecClienteTabPreventa(tabCliente);
+                                activity.setCreditoLimiteClientePreventa(creditoLimiteClienteP);
+                                activity.setDueClientePreventa(dueClienteP);
+
+                                //TODO MODIFICAR CON EL ID CONSECUTIVOS
+                                activity.setInvoiceIdPreventa(1);
+                                activity.setMetodoPagoClientePreventa(metodoPagoId);
+                            }
+                        })
+                        .setNegativeButton("Cancel",
+                                new DialogInterface.OnClickListener() {
+
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+
+
+
+                AlertDialog alertD = alertDialogBuilder.create();
+                alertD.show();
+
+
+                //  Toast.makeText(QuickContext, "You clicked FACTURA NUEVA " + factura_nueva.getP_id(), Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+        if (selected_position == position) {
+            holder.cardView.setBackgroundColor(Color.parseColor("#607d8b"));
+        }
+        else {
+            holder.cardView.setBackgroundColor(Color.parseColor("#009688"));
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return contentList.size();
+    }
+
+    public class CharacterViewHolder extends RecyclerView.ViewHolder {
+
+
+        private TextView txt_prev_card, txt_prev_fantasyname, txt_prev_companyname, txt_prev_creditlimit,
+                txt_prev_fixeddescount, txt_prev_due, txt_prev_credittime;
+        public CardView cardView;
+
+
+        public CharacterViewHolder(View view) {
+            super(view);
+            cardView = (CardView) view.findViewById(R.id.cardViewVentaDirectaClientes);
+
+            txt_prev_card = (TextView) view.findViewById(R.id.txt_ventadir_card);
+            txt_prev_fantasyname = (TextView) view.findViewById(R.id.txt_ventadir_fantasyname);
+            txt_prev_companyname = (TextView) view.findViewById(R.id.txt_ventadir_companyname);
+
+            txt_prev_creditlimit = (TextView) view.findViewById(R.id.txt_ventadir_creditlimit);
+            txt_prev_fixeddescount = (TextView) view.findViewById(R.id.txt_ventadir_fixeddescount);
+            txt_prev_due = (TextView) view.findViewById(R.id.txt_ventadir_due);
+            txt_prev_credittime = (TextView) view.findViewById(R.id.txt_ventadir_credittime);
+        }
+    }
+
+    /* private int getContador() {
+         Realm realm = Realm.getDefaultInstance();
+         realmResultado = realm.where(InvoiceDetallePreventa.class).findAll();
+         realmResultado.sort("p_id");
+
+ //        InvoiceDetallePreventa invoiceDetallePreventa = activity.getCurrentInvoice();
+ //        invoiceDetallePreventa.setP_code(weqweq);
+ //
+         return realmResultado.size();
+     }
+ */
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+}
+
